cf71e9f059a223c5af169699a3c6a017546c16ee
Nuno Cruz <nuno.cruz@sky.uk>
2021-03-04 09:25:46 +0000

    Update db location

--- client/lib/db.js
+++ client/lib/db.js
@@ -1,7 +1,7 @@
 import lowdb from 'lowdb';
 import FileSync from 'lowdb/adapters/FileSync';
 
-const db = lowdb(new FileSync("../data/db.json"));
+const db = lowdb(new FileSync("./data/db.json"));
 
 db.defaults({ results: []}).write();
 
