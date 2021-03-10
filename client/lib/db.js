713e00b137095580ce8ae794e4426c40787d1fab
Nuno Cruz <nunocruz@Nunos-MBP.lan>
2021-03-10 20:26:15 +0000

    Adds filters by location

--- client/lib/db.js
+++ client/lib/db.js
@@ -1,6 +1,18 @@
 import houses from "../data/db.json";
 
 export function getHouses() {
-  return houses.results;
+  return houses.results.sort((a, b) => b.parseDate - a.parseDate);
+}
+
+export function getAvailableLocations() {
+  const locations = [];
+
+  houses.results.forEach(({ location }) => {
+    if (!locations.includes(location)) {
+      locations.push(location);
+    }
+  });
+
+  return locations;
 }
 
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,8 @@
-import { getHouses } from '../lib/db';
+import { useState } from 'react';
+import { getAvailableLocations, getHouses } from '../lib/db';
 import style from '../styles/Home.module.css'
 
-function HouseItem({ link, title, price, location, photo }) {
+function HouseItem({ link, title, price, location, photo, parseDate }) {
   return (
     <li className={style.houseItem}>
       <a href={link} target="_blank" rel="noopener noreferrer">
@@ -12,21 +13,59 @@ function HouseItem({ link, title, price, location, photo }) {
         </div>
         <div className={style.houseItemMetadata}>
           <h3>{title}</h3>
-          <p>{price}€</p>
-          <p>{location}</p>
+          <p>💰 {price}€</p>
+          <p>📍 {location}</p>
+          <p>🗓 {new Date(parseDate).toDateString()}</p>
         </div>
       </a>
     </li>
   );
 }
 
-export default function Home({ houses = [] }) {
+function LocationFilters({ locations = [], onClick, activeFilters }) {
+  return (
+    <ul className={style.locationFilterList}>
+      {locations.map(location => {
+        const isActive = activeFilters.includes(location);
+
+        return (
+          <li key={location}>
+            <button
+              className={`${style.locationFilter} ${isActive ? style.locationFilterActive : ''}`}
+              onClick={() => onClick(location)}>
+                {location}
+            </button>
+          </li>
+      )})}
+    </ul>
+  )
+}
+
+export default function Home({ houses = [], locations = [] }) {
+  const [activeLocations, setActiveLocations] = useState([]);
+  const filteredHouses = activeLocations.length > 0 ? 
+    houses.filter(({ location }) => activeLocations.includes(location)) :
+    houses;
+
+  const onClickLocation = (location) => {
+    const locationIndex = activeLocations.indexOf(location);
+
+    if (locationIndex > -1) {
+      setActiveLocations([...activeLocations.slice(0, locationIndex), ...activeLocations.slice(locationIndex + 1)]);
+    } else {
+      setActiveLocations([...activeLocations, location]);
+    }
+  }
+
   return (
     <>
-    <h1 className={style.title}>Houses List</h1>
-    <ul className={style.houseList}>
-      {houses.map(({ id, link, title, price, location, photo }) => (<HouseItem key={id} link={link} title={title} price={price} location={location} photo={photo} />))}
-    </ul>
+      <h1 className={style.title}>Lista de Casas</h1>
+      <LocationFilters locations={locations} activeFilters={activeLocations} onClick={onClickLocation}/>
+      <ul className={style.houseList}>
+        {filteredHouses.map(({ id, link, title, price, location, photo, parseDate }) => (
+          <HouseItem key={id} link={link} title={title} price={price} location={location} photo={photo} parseDate={parseDate} />
+        ))}
+      </ul>
     </>
   )
 }
@@ -34,7 +73,8 @@ export default function Home({ houses = [] }) {
 export async function getStaticProps() {
   return {
     props: {
-      houses: getHouses()
+      houses: getHouses(),
+      locations: getAvailableLocations()
     }
   }
 }
