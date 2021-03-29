3454d671fc81bb48d397e514ae7fbcb65535b1b6
Nuno Cruz <nuno.cruz@sky.uk>
2021-03-29 10:29:35 +0100

    Initial commit

--- scraper/src/index.js
+++ scraper/src/index.js
@@ -9,7 +9,12 @@ const Logger = require('js-logger');
 require('dotenv').config();
 
 (async () => {
-  const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
+  //const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
+  const config = {
+    urls: [
+      { source:"SAPO", url: "https://casa.sapo.pt/comprar-apartamentos/t2-ate-t6-ou-superior/lisboa/?gp=260000&ly=2000", location: 'Lisboa'}
+    ]
+  };
   const urls = config.urls;
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -35,7 +40,20 @@ require('dotenv').config();
     Logger.info(`Parsing ${i+1} of ${urls.length}...`);
 
     const url = urls[i];
-    const parsedResults = await parseURL(page, db, url); 
+    let parsedResults;
+    
+    switch (url.source) {
+      case 'IMOVIRTUAL': 
+        parsedResults = await parseURL(page, db, url);
+        break;
+      case 'SAPO':
+        parsedResults = await parseSapoURL(page, db, url);
+        break;
+      default:
+        Logger.info('Unknown source');
+    }
+
+    console.log(parsedResults);
 
     if (Array.isArray(parsedResults)) {
       const filteredResults = parsedResults.filter(item => {
@@ -54,6 +72,44 @@ require('dotenv').config();
 
   Logger.info("Parsing process ended.");
 })();
+
+async function parseSapoURL(page, db, url) {
+  await page.goto(url.url, {waitUntil: 'load', timeout: 0});
+
+  try {
+    await page.waitForSelector('#divSearchPageResults');
+    Logger.info("Found results.");
+  } catch {
+    Logger.info("Results not found.");
+    return [];
+  }
+
+
+  return await page.$$eval('#divSearchPageResults .searchResultProperty', (results, location) => {
+    let newResults = [];
+
+    for(result of results) {
+      const titleElem = result.querySelector('.titleG3');
+      const priceElem = result.querySelector('.searchPropertyPrice span');
+      const linkElem = result.querySelector('a');
+      const photoElem = result.querySelector('.photoContainer img');
+
+      const obj = {
+        title: titleElem.textContent,
+        price: parseInt(priceElem.textContent.replace('\n', '').replace(/ /g, '').replace('€', '').trim()),
+        link: linkElem.href,
+        id: `imovirtual_${result.dataset.dataUid}`,
+        photo: photoElem.dataset.src,
+        location: location,
+        parseDate: new Date().getTime()
+      };
+
+      newResults.push(obj);
+    }
+
+    return newResults;
+  }, url.location);
+}
 
 async function parseURL(page, db, url) {
   await page.goto(url.url, {waitUntil: 'load', timeout: 0});
